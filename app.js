a0020def9b79bbcf672c444f3d540c81581cf5cd
Edward Mercado <emercadodt@unicartagena.edu.co>
2025-02-16 06:43:28 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ let amigos = [];
 
 function agregarAmigo() {
   let input = document.getElementById('amigo').value;
-  if (input === ""){
+  if (input === "") {
     alert('Por favor, inserte un nombre');
     return;
   }
   amigos.push(input);
 
   limpiarCaja();
-
+  listarAmigos();
 }
 
 function limpiarCaja() {
   let valorCaja = document.querySelector('#amigo')
   valorCaja.value = '';
+}
+
+function listarAmigos() {
+  const listaAmigos = document.getElementById('listaAmigos');
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    // Crear un nuevo elemento de lista para cada amigo
+    let li = document.createElement('li');
+    li.textContent = amigos[i];
+    listaAmigos.appendChild(li);
+  }
 }
