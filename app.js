bb95282e064d150d0c6754535c9ee5fe70986ca6
Edward Mercado <emercadodt@unicartagena.edu.co>
2025-02-15 19:26:30 -0500

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let input = document.getElementById('amigo').value;
+  if (input === ""){
+    alert('Por favor, inserte un nombre');
+    return;
+  }
+  amigos.push(input);
+
+  limpiarCaja();
+
+}
+
+function limpiarCaja() {
+  let valorCaja = document.querySelector('#amigo')
+  valorCaja.value = '';
+}
